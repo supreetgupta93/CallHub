ba67dc3ee8a3704aba5d4b114c7caa2c1bd2f684
supreetgupta93 <supreetgupta93@gmail.com>
2025-08-31 11:59:03 +0530

    Initial commit - CallHub project

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,52 @@
+import { Link, useNavigate } from 'react-router-dom';
+import "../App.css";
+export default function LandingPage() {
+
+
+    const router = useNavigate();
+
+    return (
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>CallHub</h2>
+                </div>
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by CallHub</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+
+                    <img src="/mobile.png" alt="" />
+
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
